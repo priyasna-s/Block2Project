cff53449b83dc51978e4908437154873da41ac9b
priyasna-s <priyasnashrestha@gmail.com>
2026-02-14 12:31:42 -0700

    Cleaning up code and fixing small errors.

--- Block2Project.py
+++ Block2Project.py
@@ -1,46 +1,52 @@
-#importing a standard for regular expressions
+# importing a standard for regular expressions
 import re
 
 
 def main():
-    # bool to check if the password is strong and for no white space
+    # bool to check if the password is strong
     strong_password = False
-    does_not_have_space = False
 
     # loop that continues until a strong password is received
-    while not strong_password and not does_not_have_space:
+    while not strong_password:
+
+        # assuming that the password is strong at first and setting it to true
+        strong_password = True
 
         # prompting the user to enter their password
         user_input = input("Enter a password: ")
 
-        # checking for an upper-case letter
+        # checking for an upper-case letter and setting strong password to false if none found
         if not re.findall(r'[A-Z]+', user_input):
             print("You need at least one upper-case letter.")
+            strong_password = False
 
-        # checking for a number
+        # checking for a number and setting strong password to false if none found
         if not re.findall(r'[0-9]+', user_input):
             print("You need at least one number.")
+            strong_password = False
 
-        # checking for a lower-case letter
+        # checking for a lower-case letter and setting strong password to false if none found
         if not re.findall(r'[a-z]+', user_input):
             print("You need at least one lower-case letter.")
+            strong_password = False
 
-        # checking for a space in the password
+        # checking for a special character (no spaces) and setting strong password to false if none found
+        if not re.findall(r'[@\+\?\(\)!\[\]#\$%\^&\*\-\=]', user_input):
+            print("You need to have at least one special character ( @+?()![]#$%^&*-= ).")
+            strong_password = False
+
+        # checking for no spaces in the password
         if re.findall(r'\s+', user_input):
             print("You cannot have any spaces in your password.")
-            does_not_have_space = True
+            strong_password = False
 
-        # checking for a special character
-        if not re.findall(r'\W+', user_input):
-            print("You need to have at least one special character ( @+?()![]#$%^&*-= )")
+        # checking the length of the password and setting strong password to false if none found
+        if len(user_input) < 13:
+            print("Your password needs to be 13+ characters.")
+            strong_password = False
 
-        # checking the length of the password
-        if len(user_input) < 13 and not re.findall(r'\s+', user_input):
-            print("Your password needs to be 13+ characters")
-        else:
-            if not does_not_have_space:
-                strong_password = True
-                print("You have a strong password :) !")
+    # if all checks pass then the password is strong
+    print("You have a strong password :) !")
 
 
 if __name__ == "__main__":
